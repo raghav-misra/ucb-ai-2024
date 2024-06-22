5d9d0e4af7b1bbc6f4a2c3ee41c90d013cfee4a7
Raghav Misra <raghav.m2014@gmail.com>
2024-06-22 16:45:28 -0700

    hide header in player

--- packages/frontend/components/AppHeader.vue
+++ packages/frontend/components/AppHeader.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
 <template>
-    <header class="bg-dark p-3 d-flex">
+    <header v-if="!route.fullPath.includes('/play')" class="bg-dark p-3 d-flex">
         <h1 class="h3 mb-0 text-white">TwizzApp</h1>
         <div class="ms-auto">
             <input type="text" class="form-control" placeholder="Search games...">
