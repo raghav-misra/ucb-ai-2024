7dd2885b83a7df2d7b4c59342278fb66b34b9cc4
Raghav Misra <raghav.m2014@gmail.com>
2024-06-22 10:29:36 -0700

    Game builder sidebar

---
+++ packages/frontend/pages/edit/[slug].vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="page d-flex align-items-stretch">
+        <div class="sidebar bg-light p-4 text-end d-flex flex-column">
+            <div class="pe-3">
+                <h2 class="lead">TwizzApp Builder</h2>
+                <small><i>"Untitled game"</i></small>
+                <hr>
+            </div>
+            <nav class="d-flex flex-column align-items-stretch flex-grow-1">
+                <button class="text-end btn btn-light">Game overview</button>
+                <button class="text-end btn btn-light">Map and locations</button>
+                <button class="text-end btn btn-light">Player attributes</button>
+                <button class="text-end btn btn-light">Tab 4</button>
+                <button class="text-end btn btn-light">Tab 5</button>
+
+                <button class="text-end btn btn-light mt-auto">Back to dashboard</button>
+            </nav>
+        </div>
+        <div class="content flex-grow-1"></div>
+    </div>
+</template>
