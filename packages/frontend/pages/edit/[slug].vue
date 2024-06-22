8f046b2d55073f886f7bce95beb588469ca1a088
Raghav Misra <raghav.m2014@gmail.com>
2024-06-22 10:39:05 -0700

    scuffed editor routing

--- packages/frontend/pages/edit/[slug].vue
+++ packages/frontend/pages/edit/[slug].vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+const view = {
+    curr: ref('overview'),
+    go(loc: string) {
+        this.curr.value = loc;
+    },
+    is(loc: string) {
+        return this.curr.value === loc;
+    }
+};
+
+const currentView = ref('overview');
+</script>
+
 <template>
     <div class="page d-flex align-items-stretch">
         <div class="sidebar bg-light p-4 text-end d-flex flex-column">
@@ -7,15 +21,21 @@
                 <hr>
             </div>
             <nav class="d-flex flex-column align-items-stretch flex-grow-1">
-                <button class="text-end btn btn-light">Game overview</button>
-                <button class="text-end btn btn-light">Map and locations</button>
-                <button class="text-end btn btn-light">Player attributes</button>
-                <button class="text-end btn btn-light">Tab 4</button>
-                <button class="text-end btn btn-light">Tab 5</button>
+                <button class="text-end btn btn-light" @click="view.go('overview')">Game overview</button>
+                <button class="text-end btn btn-light" @click="view.go('locations')">Map and locations</button>
+                <button class="text-end btn btn-light" @click="view.go('attributes')">Player attributes</button>
+                <button class="text-end btn btn-light" @click="view.go('npcs')">Non-playable characters</button>
+                <button class="text-end btn btn-light" @click="view.go('outcomes')">Game outcomes</button>
 
                 <button class="text-end btn btn-light mt-auto">Back to dashboard</button>
             </nav>
         </div>
-        <div class="content flex-grow-1"></div>
+        <div class="content flex-grow-1 p-4">
+            <div v-if="view.is('overview')">overview</div>
+            <div v-if="view.is('locations')">locations</div>
+            <div v-if="view.is('attributes')">attributes</div>
+            <div v-if="view.is('npcs')">npcs</div>
+            <div v-if="view.is('outcomes')">outcomes</div>
+        </div>
     </div>
 </template>
