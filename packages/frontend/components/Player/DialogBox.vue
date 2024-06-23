3b8c2ae64fe80246965bb879083d5b20502240de
Raghav Misra <raghav.m2014@gmail.com>
2024-06-22 19:16:45 -0700

    add dialog box and init colyseus

---
+++ packages/frontend/components/Player/DialogBox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="dialog-box-container">
+        <div class="dialog-box p-4 bg-black text-center">
+            <h2>THE WORLD 🌎</h2>
+            <h4 class="fst-italic">You failed to examine your glock. You don't even own a glock!</h4>
+
+            <button class="btn btn-black btn-lg">[ okay ]</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.dialog-box-container {
+    position: absolute;
+    bottom: 2rem;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+}
+
+.dialog-box {
+    width: 75%;
+    border: 2px white solid;
+    box-shadow: -0.5rem -0.5rem 2rem black, 0.5rem 0.5rem 2rem black;
+}
+</style>
--- packages/frontend/pages/play/[slug].vue
+++ packages/frontend/pages/play/[slug].vue
@@ -1,3 +1,65 @@
 <template>
-    
+    <section class="region d-flex align-items-center justify-content-center">
+        <div class="relaxed-box bg-black">
+            <div class="city"></div>
+            <div style="position: relative;">
+                <textarea style="resize: none;" class="lead input-box mb-0 bg-black p-4"></textarea>
+                <span class="enter-icon text-white d-flex align-items-center">↵</span>
+            </div>
+        </div>
+
+        <PlayerDialogBox />
+    </section>
 </template>
+
+<style>
+.region * {
+    font-family: "VT323", sans-serif !important;
+    color: white !important;
+}
+</style>
+
+<style scoped>
+.region {
+    width: 100%;
+    height: 100%;
+    background-image: url(~/assets/region.png);
+    background-position: center center;
+    background-size: cover;
+    position: relative;
+}
+
+.relaxed-box {
+    width: 25rem;
+    box-shadow: -0.5rem -0.5rem 2rem black, 0.5rem 0.5rem 2rem black;
+    display: flex;
+    flex-direction: column;
+}
+
+.input-box {
+    border: none;
+    outline: none;
+    color: white;
+    width: 100%;
+    padding-right: 3rem !important;
+    height: 5rem;
+    line-height: 1.25rem;
+}
+
+.enter-icon {
+    position: absolute;
+    bottom: 0;
+    top: 0;
+    right: 1rem;
+    pointer-events: none;
+    font-size: 2rem;
+}
+
+.city {
+    width: 25rem;
+    height: 25rem;
+    background-image: url(~/assets/city.png);
+    background-position: center center;
+    background-size: cover;
+}
+</style>
